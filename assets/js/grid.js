eb577fb8f99017fae8f91417aa608957cb020c66
Ivan Zdane <izdane@gmail.com>
2018-02-02 14:06:40 -0500

    Began work on proper path offest calculations

--- assets/js/grid.js
+++ assets/js/grid.js
@@ -7,7 +7,13 @@
 var lastClicked;
 var numberOfRows = 0;
 var points= [];
-var scaleValue = 1.25;
+var scaleValue = 2.417;
+var offset = 0.025;
+
+var leftVector = new Victor(0,0);
+var rightVector = new Victor(0,0);
+
+//Encoder 0.0001114 m pulse^-1
 
 //Create the grid variable, takes parameters for rows, columns and the clicakbleGrid function
 var grid = clickableGrid(10,20,function(box,row,col){
@@ -22,7 +28,7 @@ var grid = clickableGrid(10,20,function(box,row,col){
 document.getElementById("table").appendChild(grid);
  
 //Function to create the grid, takes parameters for rows, columns and callback
-function clickableGrid( rows, cols, callback ){
+function clickableGrid( rows, cols, callback){
 	var i=0;
 	var grid = document.createElement('table');
 	grid.className = 'grid';
@@ -48,24 +54,84 @@ function calculate(){
 		d = c +1;
 		var futurePoint = points[d];
 
-		console.log(currentPoint.distance(futurePoint));
-
 		//directionVector = new Victor(-(currentPoint.x - futurePoint.x), -(currentPoint.y - futurePoint.y));
 		outputToFile(currentPoint.x, currentPoint.y);
+    offset_calculate(1.0,1.0,2.0,2.0);
 	}
 }
 
+
+function offset_calculate(x1,y1,x2,y2){
+  //Inital Point
+  var Fraction = algebra.Fraction
+  var Expression = algebra.Expression;
+  var Equation = algebra.Equation;
+  var inital_point = new Victor(x1, y1);
+  var next_point = new Victor(x2, y2);
+
+  var AB = new Victor((x2-x1), (y2-y1));
+
+  var abx = AB.x;
+  var aby = AB.y;
+
+  var adx = algebra.parse("x");
+  adx = adx.subtract(x1);
+  var ady = algebra.parse("y")
+  ady = ady.subtract(y1);
+
+  var expression1 = adx.add(ady);
+  var equation_1 = new Equation(expression1, 0);
+  var equation_12 = equation_1.solveFor("x");
+
+  var adx2 = adx.multiply(adx);
+  var ady2 = ady.multiply(ady);
+
+  var expression3 = adx2.add(ady2);
+  var expression4 = expression3.eval({x : equation_12});
+
+  //INSERT SCALE VALUE HERE
+  var  equation5 = new Equation(expression4, 4);
+  var roots = equation5.solveFor("y");
+  var xroot = new Fraction(parseInt(roots[0] * 1000000), 1);
+
+  console.log(xroot);
+
+
+
+  console.log("Equation 1 is " + equation_1);
+
+  var x_1 = equation_1.solveFor("x");
+  console.log("Our re-arranged equaiton is " + x_1);
+  
+
+ var x_11 = x_1.eval({y: xroot});
+ var x_111 = x_11 / 1000000
+
+  console.log("The final result is " + x_111);
+
+}
+
+//CSV Output Code
 itemsNotFormatted = [];
 function outputToFile(xi,yi){
-	for (var e = 0; e < 10; ++e){
+	for (var e = 0; e < 100; ++e){
 		var scaledX = xi * scaleValue;
 		var scaledY = yi * scaleValue;
 
-		var leftY = scaledY + 0.025;
-		var rightY = scaledY - 0.025;
+		var leftY = scaledY + offset;
+		var rightY = scaledY - offset;
 
 		var xPath = 0;
 		var yPath = 0;
+
+		var tempX = new Victor(scaledX, leftY);
+		var tempY = new Victor(scaledX, rightY);
+
+		xPath = leftVector.distance(tempX);
+		yPath = rightVector.distance(tempY);
+
+		leftVector = tempX;
+		rightVector = tempY;
 
 		itemsNotFormatted.push({
 			driveLeftX: scaledX,
@@ -152,7 +218,7 @@ function download(){
       });
   });
 
-  var fileTitle = 'atuo'; 
+  var fileTitle = 'auto'; 
 
   exportCSVFile(headers, itemsFormatted, fileTitle); // call the exportCSVFile() function to process the JSON and trigger the download
 }
