6799b90f4999c97a56aa80f002400facf90b6c21
Ivan Zdane <izdane@gmail.com>
2018-02-24 15:08:33 -0500

    Added scalar for turns

--- assets/js/grid.js
+++ assets/js/grid.js
@@ -212,16 +212,13 @@ function calculate(){
       var current_left_output;
       var current_right_output;
       if(left_current_position > right_current_position){
-        current_left_output = left_outputs[x] * 1.5;
-
+        current_left_output = left_outputs[x] * 2.25;
       } else if (right_current_position > left_current_position){
-        current_right_output = right_outputs[x] * 1.5;
-
+        current_right_output = right_outputs[x] * 2.25;
       } else {
         current_left_output = left_outputs[x];
         current_right_output = right_outputs[x];
       }
-
 
       console.log(current_left_output + "," + current_right_output + ",," + left_position + "," + right_position);
       
